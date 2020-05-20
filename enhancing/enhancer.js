81bb4dfdf3c896c35d3b8cec3264812a0f3a1e3d
janecyyu <janecyyu@gmail.com>
2020-05-20 12:53:51 -0700

    add jest

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,8 +5,15 @@ module.exports = {
   get,
 };
 
+let item =
+  {
+    name: "Tom",
+    durability: 50,
+    enhancement: 15
+  }
+
 function succeed(item) {
-  return { ...item };
+  return { ...item};
 }
 
 function fail(item) {
@@ -14,7 +21,7 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability = 100 };
 }
 
 function get(item) {
